42c680439e7a9a166bc72c4adb3d338aafbb5225
Kato <zzeroyzz@gmail.com>
2024-10-06 15:45:58 -0400

    feat: "Integrated Snake Game iframe and fine-tuned layout for window display."

---
+++ src/components/SnakeGame/SnakeGameIframe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SnakeGameIframe = () => {
+  return (
+    <iframe
+      src="https://zzeroyzz.github.io/snakegame/"
+      title="Snake Game"
+      width="100%"
+      height="540px"
+      style={{ border: 'none' }}
+    />
+  );
+};
+
+export default SnakeGameIframe;
