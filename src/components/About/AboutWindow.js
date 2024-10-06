1966bf9bfc23bb8c559547b3e626ad8a330adb8b
Kato <zzeroyzz@gmail.com>
2024-10-06 16:00:57 -0400

    feat: Implemented responsive design for the AboutWindow component using Chakra UI's useBreakpointValue. Added dynamic sizing for mobile and desktop views, integrated the WindowBox component for window behavior, and enhanced UI with an image and descriptive content from aboutData.

---
+++ src/components/About/AboutWindow.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Image, Text, VStack, Flex, Box, useBreakpointValue } from '@chakra-ui/react';
+import "@lottiefiles/lottie-player";
+import kato from '../../assets/kato.png';
+import aboutData from './constants';
+import WindowBox from '../WindowBox/WindowBox';
+
+const AboutWindow = ({isWindowOpen, toggleWindow, zIndex, bringToFront}) => {
+  const windowSize = useBreakpointValue({
+    base: { width: "380px", height: "400px" },
+    md: { width: "500px", height: "600px" },
+    lg: { width: "700px", height: "600px" },
+  });
+  return (
+    <>
+      {isWindowOpen && (
+        <WindowBox
+          title="About"
+          minWidth={windowSize.width}
+          minHeight={windowSize.height}
+          backgroundColor="white"
+          content={
+            <VStack spacing={3} backgroundColor="white" p={3}>
+              <Box backgroundColor="pink" borderRadius='full'>
+              <Image src={kato} boxSize={40} alt="About"  borderRadius='full'/>
+</Box>
+              {aboutData.map((service, index) => (
+                <Flex key={index} textAlign="left" flexDirection="column" gap={2}>
+                  <Text fontWeight="bold">{service.title}</Text>
+                  <Text fontSize="md">{service.description}</Text>
+                </Flex>
+              ))}
+            </VStack>
+          }
+          onClose={toggleWindow}
+          zIndex={zIndex}
+          bringToFront={bringToFront}
+        />
+      )}
+    </>
+  );
+};
+
+export default AboutWindow;
