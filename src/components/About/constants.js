0ea0ec89def685ae3a0f2674f1daeb703c0097e0
Kato <zzeroyzz@gmail.com>
2024-10-06 16:00:04 -0400

    feat: created constants for about component

---
+++ src/components/About/constants.js
@@ -0,0 +1,21 @@
+
+const aboutData = [
+  {
+    title: "A little about me",
+    description: "When I was younger, my parents had an old-school Atari, and that's where my fascination with technology began. I fell in love with 8-bit games and would beg my dad to help me figure out how they were made. Although the language behind 8-bit games was too complex for me at the time (I later learned it was assembly language), I didn't give up. I started with HTML and gradually worked my way up. From there, I built games and websites for my friends, and that's where I discovered my passion for design, animation, and web development."
+  },
+  {
+    title: "",
+    description: "I loved how I could bring whatever I envisioned onto the screen, creating something people could interact with. I was particularly inspired by Apple and their beautifully designed, structured websites. The experience of visiting Apple's site filled me with excitement, and I wanted to create that same feeling for others—designing digital experiences that resonate deeply."
+  },
+  {
+    title: "",
+    description: "Growing up in Atlanta, I always loved learning and challenging myself. I used to take apart computers and remotes just to understand how they worked, only to put them back together. Over time, this curiosity led me to freelancing, where I worked with local businesses to improve their websites, build custom animations, and create unique digital solutions. I found that the joy I got from solving problems and bringing ideas to life was something I wanted to pursue full-time."
+  },
+  {
+    title: "",
+    description: "Today, my love for technology, design, and learning continues to grow. Whether it's developing complex web applications or crafting visually stunning animations, I'm always eager to push the boundaries of what's possible."
+  },
+];
+
+export default aboutData;
